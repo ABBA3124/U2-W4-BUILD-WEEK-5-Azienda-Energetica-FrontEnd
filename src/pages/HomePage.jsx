f20d4f68f2631d7ac869cd7b7e3da4c71b84ff0f
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-10 15:40:09 +0200

    Add homepage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+  return (
+    <div className="text-center">
+      <div>
+        <h1>ciao, sono la HomePage</h1>
+        <p>Il mio creatore è giuseppe detto peppino</p>
+        <p>ciccio cristiano è bello </p>
+        <p>luca di pietro è più bravo di riccardo </p>
+        <p>davide non vede l'ora di finire la buildweek</p>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
